d718e870cea322dd5de9777791c3c0c8e32f1420
jlangr <jeff@langrsoft.com>
2013-03-28 12:04:53 -0600

    6 fix

--- WavReaderTest.cpp
+++ WavReaderTest.cpp
@@ -1,3 +1,4 @@
+// START:test
 #include "CppUTest/TestHarness.h"
 
 #include "WavReader.h"
@@ -10,6 +11,7 @@ using namespace std;
 TEST_GROUP(WavReader) {
 };
 
+// END:test
 TEST(WavReader, HasExtension) {
    string s{"a.bcd"};
    CHECK(!hasExtension(s, "xxxx"));
@@ -25,31 +27,13 @@ TEST_GROUP(WavReader_WriteSamples)
    ostringstream out;
 };
 
-void writeSamples(ostream* out, char* data, 
-// END_HIGHLIGHT
-      uint32_t startingSample, 
-      uint32_t samplesToWrite, 
-      uint32_t bytesPerSample) {
-//   rLog(channel, "writing %i samples", samplesToWrite);
-
-   for (auto sample = startingSample; 
-        sample < startingSample + samplesToWrite; 
-        sample++) {
-      auto byteOffsetForSample = sample * bytesPerSample;
-      for (uint32_t byte{0}; byte < bytesPerSample; byte++) 
-// START_HIGHLIGHT
-         out->put(data[byteOffsetForSample + byte]);
-// END_HIGHLIGHT
-   }
-}
-
 TEST(WavReader_WriteSamples, WritesSingleSample) {
    char data[] { "abcd" };
    uint32_t bytesPerSample { 1 };
    uint32_t startingSample { 0 };
    uint32_t samplesToWrite { 1 };
 
-   /* reader.*/writeSamples(&out, data, startingSample, samplesToWrite, bytesPerSample);
+   reader.writeSamples(&out, data, startingSample, samplesToWrite, bytesPerSample);
 
    CHECK_EQUAL("a", out.str());
 }
@@ -60,7 +44,7 @@ TEST(WavReader_WriteSamples, WritesMultibyteSampleFromMiddle) {
    uint32_t startingSample { 4 };
    uint32_t samplesToWrite { 3 };
 
-   /*reader.*/writeSamples(&out, data, startingSample, samplesToWrite, bytesPerSample);
+   reader.writeSamples(&out, data, startingSample, samplesToWrite, bytesPerSample);
 
    CHECK_EQUAL("89ABCD", out.str());
 }
