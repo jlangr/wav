33073c46a9030c84301969f95c5fe7eab5d26425
jlangr <jeff@langrsoft.com>
2013-03-28 13:49:56 -0600

    done

--- rlog/RLogChannel.cpp
+++ rlog/RLogChannel.cpp
@@ -4,19 +4,16 @@
 
 namespace rlog
 {
-   RLogChannel::RLogChannel( const std::string &name, LogLevel level ){ }
+RLogChannel::RLogChannel( const std::string &name, LogLevel level ){ }
+RLogChannel::~RLogChannel(){}
+void RLogChannel::publish(const RLogData &data){}
 
-	RLogChannel::~RLogChannel(){}
-	void RLogChannel::publish(const RLogData &data){}
+std::string nameReturn("");
+const std::string &RLogChannel::name() const { return nameReturn; }
 
-   std::string nameReturn("");
-	const std::string &RLogChannel::name() const { return nameReturn; }
-
-	LogLevel RLogChannel::logLevel() const { return LogLevel(); }
-	void RLogChannel::setLogLevel(LogLevel level) {}
-
-	RLogChannel *getComponent(RLogChannel *componentParent, 
-		                  const char *component){ return 0; }
-
+LogLevel RLogChannel::logLevel() const { return LogLevel(); }
+void RLogChannel::setLogLevel(LogLevel level) {}
+RLogChannel *getComponent(RLogChannel *componentParent, 
+                     const char *component){ return 0; }
 }
 
