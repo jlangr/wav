718b90a0a9e4f214d35e186bc8460933f82d794d
jlangr <jeff@langrsoft.com>
2013-04-01 13:41:38 -0600

    revert to minus 1

--- Snippet.h
+++ Snippet.h
@@ -5,13 +5,6 @@ using namespace std;
 
 class Snippet {
 public:
-   uint32_t dataLength(
-         uint32_t samples, 
-         uint32_t bytesPerSample,
-         uint32_t channels
-         ) const {
-      return samples * bytesPerSample * channels;
-   }
 };
 
 #endif
