58c1fbc788b7b0fc386ebd085f7fff91f5bd2f98
jlangr <jeff@langrsoft.com>
2013-04-01 13:42:59 -0600

    19

--- Snippet.h
+++ Snippet.h
@@ -5,6 +5,13 @@ using namespace std;
 
 class Snippet {
 public:
+   uint32_t dataLength(
+         uint32_t samples, 
+         uint32_t bytesPerSample,
+         uint32_t channels
+         ) const {
+      return samples * bytesPerSample * channels;
+   }
 };
 
 #endif
